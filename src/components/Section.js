d1933d5e9e451539031a8c4011d2f49206a5158b
Oliver <oliver.vukovic@quantoxtechnology.com>
2021-11-12 19:20:12 +0100

    add cart calculate proces

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,13 +3,13 @@ import Product from "./Product";
 
 
 export default function Section(props) {
-    const {products} = props;
+    const { products, onAdd } = props;
     return (
         <section>
             <h2>Choose our Products</h2>
             <div className="row">
                 {products.map((product) => (
-                    <Product key={product.id} product={product}>
+                    <Product key={product.id} product={product} onAdd={onAdd}>
                     </Product>
                 ))} 
             </div>
